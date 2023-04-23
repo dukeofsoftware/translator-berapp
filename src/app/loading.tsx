e4928322b24fe2621404042cec7d482c004e25b6
dukeofsoftware <kozanfurkanemre@gmail.com>
2023-04-23 14:30:00 +0300

    fix

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import { Loader } from "@mantine/core"
 
 export default function Loading() {
   return (
-    <div className="h-full w-full flex items-center justify-center gap-12">
+    <div className="h-screen w-full flex items-center justify-center gap-24">
       <Loader size="xl" variant="dots" />;
       <Loader size="xl" variant="dots" />;
       <Loader size="xl" variant="dots" />;
